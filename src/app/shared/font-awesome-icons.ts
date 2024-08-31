f0e423e3b0fa4dd8e1d6e6a897a2f7d75389dfb5
nassimguelai <nassim.guelai@gmail.com>
2024-08-31 19:02:20 +0200

    initialisation du projet angular

---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,8 @@
+import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
+import {faUser} from "@fortawesome/free-regular-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faAirbnb,
+  faUser
+];
